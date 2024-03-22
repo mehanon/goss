69527f968ec2d5b3faefa202f62b42cedf3e8f81
heilkit <helikit@ya.ru>
2024-03-22 11:31:54 +0300

    add more nuanced transpotter

--- shrimp/balancer.go
+++ shrimp/balancer.go
@@ -5,6 +5,9 @@ import (
 	"github.com/heilkit/tg"
 	"github.com/heilkit/tg/scheduler"
 	"math/rand/v2"
+	"net"
+	"net/http"
+	"time"
 )
 
 type Balancer struct {
@@ -15,12 +18,29 @@ func NewBalancer(tokens ...string) (*Balancer, error) {
 	bots := make([]*tg.Bot, len(tokens))
 	for i, token := range tokens {
 		bot, err := tg.NewBot(tg.Settings{
-			URL:       telegramapi(),
-			Token:     token,
-			Local:     tg.LocalMoving(),
-			OnError:   tg.OnErrorLog(),
+			URL:     telegramapi(),
+			Token:   token,
+			Local:   tg.LocalMoving(),
+			OnError: tg.OnErrorLog(),
+			Client: &http.Client{
+				Transport: &http.Transport{
+					Proxy: http.ProxyFromEnvironment,
+					DialContext: (&net.Dialer{
+						Timeout:   300 * time.Second,
+						KeepAlive: 300 * time.Second,
+					}).DialContext,
+					ForceAttemptHTTP2:     true,
+					MaxIdleConns:          100,
+					IdleConnTimeout:       330 * time.Second,
+					TLSHandshakeTimeout:   10 * time.Second,
+					ExpectContinueTimeout: 2 * time.Second,
+				},
+				CheckRedirect: nil,
+				Jar:           nil,
+				Timeout:       0,
+			},
 			Scheduler: scheduler.ExtraConservative(),
-			Retries:   10,
+			Retries:   5,
 		})
 		if err != nil {
 			return nil, fmt.Errorf("creating balancer %s %v", token, err)
